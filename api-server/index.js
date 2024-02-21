90cbfc37f7e4448b3374d540be22039b34cf74b0
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-02-21 22:28:41 +0530

    api server setup

--- api-server/index.js
+++ api-server/index.js
@@ -7,8 +7,7 @@ const PORT = 9000;
 const ecsClient = new ECSClient({
     region: "ap-south-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  
   },
 });
 
