c4306e8e77abd341722bb27e2380e72b2472d497
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-02-21 16:40:24 +0530

    s3 reverse proxy setup

--- server/script.js
+++ server/script.js
@@ -5,10 +5,9 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mimeType = require("mime-types");
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: "ap-south-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+   
   },
 });
 
@@ -18,7 +17,7 @@ async function init() {
   console.log("Starting server...");
 
   const outDirPath = path.join(__dirname, "output");
-
+   
   const p = exec(`cd ${outDirPath} && npm install && npm run build`);
   p.stdout.on("data", function (data) {
     console.log(data.toString());
@@ -30,16 +29,17 @@ async function init() {
 
   p.on("close", async function () {
     console.log(`Build completed.`);
-    const distFolderPath = path.join(outDirPath, "output", "dist");
+    const distFolderPath = path.join(__dirname, "output", "dist");
     const distFolderContents = fs.readdirSync(distFolderPath, {
       recursive: true,
     });
-    for (const filePath of distFolderContents) {
+    for (const file of distFolderContents) {
+      const filePath = path.join(distFolderPath, file);
       if (fs.lstatSync(filePath).isDirectory()) continue;
       console.log("Uploading", filePath);
       const command = new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `__output/${PROJECT_ID}/${filePath}`,
+        Bucket: "hosthub-bucket",
+        Key: `__output/${PROJECT_ID}/${file}`,
         Body: fs.createReadStream(filePath),
         ContentType: mimeType.lookup(filePath),
       });
