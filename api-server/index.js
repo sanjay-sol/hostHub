4abf90ba5a4c75a067e0fe5d89e03c0216611b7b
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-02-22 18:58:06 +0530

    logs listen and publish sockets/redis

--- api-server/index.js
+++ api-server/index.js
@@ -4,6 +4,23 @@ const { generateSlug } = require("random-word-slugs");
 require("dotenv").config();
 const app = express();
 const PORT = 9000;
+const Redis = require("ioredis");
+const { Server } = require("socket.io");
+
+const subscriber = new Redis(
+  process.env.REDIS_URL
+);
+
+const io = new Server({ cors: "*" });
+
+io.on("connection", (socket) => {
+  socket.on("subscribe", (channel) => {
+    socket.join(channel);
+    socket.emit("message", `Joined ${channel}`);
+  });
+});
+
+io.listen(9002, () => console.log("Socket Server 9002"));
 
 const ecsClient = new ECSClient({
   region: "ap-south-1",
@@ -20,9 +37,9 @@ const config = {
 
 app.use(express.json());
 
-app.use("/project",async (req, res) => {
-  const { gitURL } = req.body;
-  const projectSlug = generateSlug();
+app.use("/project", async (req, res) => {
+  const { gitURL, slug } = req.body;
+  const projectSlug = slug ? slug : generateSlug();
 
   //! Spin the container by api call
   const command = new RunTaskCommand({
@@ -59,8 +76,20 @@ app.use("/project",async (req, res) => {
       ],
     },
   });
-    await ecsClient.send(command);
-    return res.json({ status : "queued" , data : { projectSlug , url : `http://${projectSlug}:8000` } });
+  await ecsClient.send(command);
+  return res.json({
+    status: "queued",
+    data: { projectSlug, url: `http://${projectSlug}:8000` },
+  });
 });
+async function initRedisSubscribe() {
+  console.log("Subscribed to logs....");
+  subscriber.psubscribe("logs:*");
+  subscriber.on("pmessage", (pattern, channel, message) => {
+    io.to(channel).emit("message", message);
+  });
+}
+
+initRedisSubscribe();
 
 app.listen(PORT, () => console.log(`Api Server Running..${PORT}`));
