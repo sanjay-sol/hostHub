330d08c1871b894cd4e9636b5d76e82a024a593e
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-02-23 21:18:55 +0530

    changed req.send url

--- api-server/index.js
+++ api-server/index.js
@@ -6,22 +6,6 @@ const cors = require("cors");
 const app = express();
 app.use(cors({ origin: "*" }));
 const PORT = 9000;
-// const Redis = require("ioredis");
-// const { Server } = require("socket.io");
-
-// const subscriber = new Redis(
-//   "rediss://default:AVNS_uwUrvXtzocWMFdio5Zi@redis-6f2c738-sanjaysirangi-1cca.a.aivencloud.com:17389"
-// );
-// const io = new Server({ cors: "*" });
-
-// io.on("connection", (socket) => {
-//   socket.on("subscribe", (channel) => {
-//     socket.join(channel);
-//     socket.emit("message", `Joined ${channel}`);
-//   });
-// });
-
-// io.listen(9002, () => console.log("Socket Server 9002"));
 
 const ecsClient = new ECSClient({
   region: "ap-south-1",
@@ -82,17 +66,8 @@ app.use("/project", async (req, res) => {
   await ecsClient.send(command);
   return res.json({
     status: "queued",
-    data: { projectSlug, url: `http://${projectSlug}:8000` },
+    data: { projectSlug, url: `http://${projectSlug}.localhost:8000` },
   });
 });
-// async function initRedisSubscribe() {
-//   console.log("Subscribed to logs....");
-//   subscriber.psubscribe("logs:*");
-//   subscriber.on("pmessage", (pattern, channel, message) => {
-//     io.to(channel).emit("message", message);
-//   });
-// }
-
-// initRedisSubscribe();
 
 app.listen(PORT, () => console.log(`Api Server Running..${PORT}`));
