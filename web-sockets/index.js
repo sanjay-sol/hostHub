f5c4731495a6299c2fed525e9e36335e4998b97e
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-02-23 19:27:21 +0530

    added cors in ws

--- web-sockets/index.js
+++ web-sockets/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors({ origin: "*" }));
 const Redis = require("ioredis");
 const { Server } = require("socket.io");
 
