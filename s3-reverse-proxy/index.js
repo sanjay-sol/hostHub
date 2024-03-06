04e7a54d38b2a1b1d17e8b057f0d84c8349b2b9f
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-06 13:25:49 +0530

    err handling and added commments

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,31 +1,44 @@
 const express = require("express");
 const httpProxy = require("http-proxy");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+const PORT = process.env.PORT || 8000;
+
 app.use(cors({ origin: "*" }));
-const PORT = 8000;
 
-const BASE_PATH = "https://hosthub-bucket.s3.ap-south-1.amazonaws.com/__output";
+const BASE_PATH = process.env.BASE_PATH;
 
+//* Create HTTP proxy
 const proxy = httpProxy.createProxy();
 
-app.get("/*", (req, res) => {
+app.get("/*", (req, res, next) => {
+
+  //! Extract hostname from request
   const hostname = req.hostname;
-  console.log("Hostname", hostname);
 
+  //! Extract subdomain from hostname
   const subdomain = hostname.split(".")[0];
-  console.log("Subdomain", subdomain);
 
+  //! Construct URL to proxy to based on subdomain
   const resolvesTo = `${BASE_PATH}/${subdomain}`;
-  console.log("Resolves To", resolvesTo);
 
-  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+  //! Proxy the request to the resolved URL
+  proxy.web(req, res, { target: resolvesTo, changeOrigin: true }, (err) => {
+    next(err);
+  });
 });
+
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
   const url = req.url;
   if (url === "/") proxyReq.path += "index.html";
 });
 
-app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
+app.use((err, req, res, next) => {
+  console.error("An error occurred:", err);
+  res.status(500).send("Internal Server Error");
+});
 
+app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
